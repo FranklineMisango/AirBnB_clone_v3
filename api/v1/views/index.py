7ed419aac00435b48819d4eb58cd119eb812cf25
Virgo Alpha <bensonkingori96@gmail.com>
2022-02-07 21:41:22 +0400

    s

---
+++ api/v1/views/index.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""The blueprint index"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review, "states": State, "users": User}
+
+
+@app_views.route('/status')
+def status():
+    """Rturn json object with the app status"""
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """Retrieve the number of each objects by type"""
+    result = {}
+    for clss in classes:
+        counter = storage.count(classes[clss])
+        result[clss] = counter
+    return jsonify(result)
